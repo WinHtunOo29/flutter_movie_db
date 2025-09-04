c8c1bd889330fef8622017b2809bafc3cf932ef2
Win Htun Oo <winhtunoo@onenex.co>
2025-09-04 23:14:12 +0700

    add movie list use case

---
+++ flutter_movie_db/lib/features/movie_list/domain/use_cases/get_top_rated_movie_list_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_movie_db/core/error/failures.dart';
+import 'package:flutter_movie_db/features/movie_list/domain/entities/movie_list_response_entity.dart';
+import 'package:flutter_movie_db/features/movie_list/domain/repositories/movie_list_repository.dart';
+
+class GetTopRatedMovieListUseCase {
+  final MovieListRepository movieListRepository;
+
+  GetTopRatedMovieListUseCase({required this.movieListRepository});
+
+  Future<Either<Failure, MovieListResponseEntity>> call() async {
+    return movieListRepository.getTopRatedMovieList();
+  }
+}
